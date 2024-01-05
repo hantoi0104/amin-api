84b4ccb08a49510d856b2b8fc25e041c48d0f227
hantoi0104 <hantoiltpt@gmail.com>
2024-01-05 21:29:24 +0700

    create repository

---
+++ src/main/kotlin/offerlinks/jpk/lss/adminapi/domain/repository/GroupsRepository.kt
@@ -0,0 +1,10 @@
+package offerlinks.jpk.lss.adminapi.domain.repository
+
+import org.springframework.stereotype.Repository
+
+@Repository
+class GroupsRepository {
+    fun getGroupsDetail(){
+
+    }
+}
